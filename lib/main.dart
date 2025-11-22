46cdb6dd2c96bc02d509ee09299bc4372b602afc
Muhammad Awais <muhammadawaisgill18@gmail.com>
2025-11-22 07:14:00 +0500

    Connected with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'package:provider/provider.dart';
 import 'package:localstorage/localstorage.dart';
 import 'providers/time_entry_provider.dart';
 import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initLocalStorage();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
